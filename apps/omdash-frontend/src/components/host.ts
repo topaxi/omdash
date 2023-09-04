fbe976e0150b898eff613500dac40553a7cedbc4
Damian Senn <damian.senn@topaxi.codes>
2023-09-04 11:02:14 +0200

    fix: webkit wants prefixed backdrop-filter

--- apps/omdash-frontend/src/components/host.ts
+++ apps/omdash-frontend/src/components/host.ts
@@ -24,6 +24,7 @@ export class OmHost extends connect()(LitElement) {
       border-radius: 6px;
       border: 1px solid var(--ctp-macchiato-lavender);
       background-color: rgb(var(--ctp-macchiato-base-raw), 66.6%);
+      -webkit-backdrop-filter: blur(8px) grayscale(1);
       backdrop-filter: blur(8px) grayscale(1);
 
       padding: 0.2rem 0.33rem;
