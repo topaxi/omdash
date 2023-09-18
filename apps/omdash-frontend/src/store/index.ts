7e17c6e44657b8f99ed0582706c3fc4beb7f22ff
Damian Senn <damian.senn@topaxi.codes>
2023-09-18 18:46:26 +0200

    fix: typings

--- apps/omdash-frontend/src/store/index.ts
+++ apps/omdash-frontend/src/store/index.ts
@@ -1,10 +1,12 @@
+import type { Store } from '@reduxjs/toolkit';
 import { wrap } from 'comlink';
 import { remoteStoreWrapper } from './remote-store';
+import type { RootState } from './worker';
 import workerUrl from './worker.js?worker&url';
 
-export type { RootState } from './worker';
+export type { RootState };
 
-const remoteStore = await wrap(
+const remoteStore = wrap<Store<RootState>>(
   new Worker(workerUrl, {
     type: 'module',
   }),
