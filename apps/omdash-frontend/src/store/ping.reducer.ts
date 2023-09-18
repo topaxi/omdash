51ebed234672bb5b6f9d6b13b622605b031a327c
Damian Senn <damian.senn@topaxi.codes>
2023-09-18 21:42:22 +0200

    fix: cleanup old pings

--- apps/omdash-frontend/src/store/ping.reducer.ts
+++ apps/omdash-frontend/src/store/ping.reducer.ts
@@ -1,4 +1,5 @@
 const MAX_PING_HISTORY = 50;
+const MAX_PING_AGE = Math.floor(MAX_PING_HISTORY * 2 + MAX_PING_HISTORY / 2);
 
 export interface PingAction {
   type: 'ping';
@@ -21,19 +22,32 @@ export interface PingState {
 
 export const initialState: PingState = {};
 
+function filterOldPings(state: PingState) {
+  const newestPing = Math.max(
+    ...Object.values(state).map((pings) => pings.at(-1)?.timestamp ?? 0),
+  );
+
+  return Object.fromEntries(
+    Object.entries(state).map(([host, pings]) => [
+      host,
+      pings.filter(({ timestamp }) => newestPing - timestamp < MAX_PING_AGE),
+    ]),
+  );
+}
+
 export function pingReducer(
   state = initialState,
   action: PingAction,
 ): PingState {
   switch (action.type) {
     case 'ping':
-      return {
+      return filterOldPings({
         ...state,
         [action.payload.host]: [
           ...(state[action.payload.host] ?? []),
           action.payload,
         ].slice(-MAX_PING_HISTORY),
-      };
+      });
     default:
       return state;
   }
