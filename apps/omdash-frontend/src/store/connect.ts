c698929eb5f87a9b05ba9309e19c5401e99e3169
Damian Senn <damian.senn@topaxi.codes>
2023-09-18 22:59:36 +0200

    refactor: move hoc into controller

--- apps/omdash-frontend/src/store/connect.ts
+++ apps/omdash-frontend/src/store/connect.ts
@@ -1,13 +1,46 @@
 import { Action, Store, Unsubscribe } from '@reduxjs/toolkit';
 import { debounce } from 'lodash-es';
 import { store as defaultStore } from './index.js';
+import { LitElement, ReactiveController, ReactiveControllerHost } from 'lit';
 
 type Constructor<T> = new (...args: any[]) => T;
 
-interface CustomElement extends HTMLElement {
-  connectedCallback?(): void;
-  disconnectedCallback?(): void;
-  readonly isConnected: boolean;
+class StoreControler implements ReactiveController {
+  host: ReactiveControllerHost & {
+    stateChanged(state: any): void;
+  };
+
+  store: Store<any, any>;
+
+  private _storeUnsubscribe: Unsubscribe | null = null;
+
+  constructor(
+    host: ReactiveControllerHost & {
+      stateChanged(state: any): void;
+    },
+    store: Store<any, any>,
+  ) {
+    (this.host = host).addController(this);
+    this.store = store;
+  }
+
+  private _triggerStateChanged() {
+    this.host.stateChanged(this.store.getState());
+  }
+
+  hostConnected() {
+    this._storeUnsubscribe = this.store.subscribe(
+      debounce(this._triggerStateChanged.bind(this), 250, {
+        maxWait: 1000,
+      }),
+    );
+
+    this._triggerStateChanged();
+  }
+
+  hostDisconnected(): void {
+    this._storeUnsubscribe?.();
+  }
 }
 
 export interface ConnectOptions<S extends Store<any, any>> {
@@ -18,31 +51,9 @@ export const connect =
   <S, A extends Action>({
     store = defaultStore as any,
   }: ConnectOptions<Store<S, A>> = {}) =>
-  <T extends Constructor<CustomElement>>(BaseElement: T) =>
+  <T extends Constructor<LitElement>>(BaseElement: T) =>
     class extends BaseElement {
-      private _storeUnsubscribe: Unsubscribe | null = null;
-
-      private _triggerStateChanged() {
-        this.stateChanged(store.getState());
-      }
-
-      connectedCallback() {
-        super.connectedCallback?.();
-
-        this._storeUnsubscribe = store.subscribe(
-          debounce(this._triggerStateChanged.bind(this), 250, {
-            maxWait: 1000,
-          }),
-        );
-
-        this._triggerStateChanged();
-      }
-
-      disconnectedCallback(): void {
-        this._storeUnsubscribe?.();
-
-        super.disconnectedCallback?.();
-      }
+      private _storeController = new StoreControler(this, store);
 
       stateChanged(_state: S): void {}
 
