9b09b541ebf3ea4fa4f6d9dfcb4ba6e2b6741f87
Damian Senn <damian.senn@topaxi.codes>
2023-09-18 18:43:45 +0200

    fix: typings

--- apps/omdash-frontend/src/store/index.ts
+++ apps/omdash-frontend/src/store/index.ts
@@ -2,6 +2,8 @@ import { wrap } from 'comlink';
 import { remoteStoreWrapper } from './remote-store';
 import workerUrl from './worker.js?worker&url';
 
+export type { RootState } from './worker';
+
 const remoteStore = await wrap(
   new Worker(workerUrl, {
     type: 'module',
