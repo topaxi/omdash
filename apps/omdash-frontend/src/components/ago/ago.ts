14716d49d4d159f59315d32cd9156a30fac4ca13
Damian Senn <damian.senn@topaxi.codes>
2023-10-27 16:40:12 +0200

    refactor: concat to template string

--- apps/omdash-frontend/src/components/ago/ago.ts
+++ apps/omdash-frontend/src/components/ago/ago.ts
@@ -15,21 +15,21 @@ function timeDifference(
   const elapsed = current - previous;
 
   if (elapsed < 1000) {
-    return elapsed + ' milliseconds ago';
+    return `${elapsed} milliseconds ago`;
   }
 
   if (elapsed < msPerMinute) {
-    return Math.round(elapsed / 1000) + ' seconds ago';
+    return `${Math.round(elapsed / 1000)} seconds ago`;
   } else if (elapsed < msPerHour) {
-    return Math.round(elapsed / msPerMinute) + ' minutes ago';
+    return `${Math.round(elapsed / msPerMinute)} minutes ago`;
   } else if (elapsed < msPerDay) {
-    return Math.round(elapsed / msPerHour) + ' hours ago';
+    return `${Math.round(elapsed / msPerHour)} hours ago`;
   } else if (elapsed < msPerMonth) {
-    return 'approximately ' + Math.round(elapsed / msPerDay) + ' days ago';
+    return `approximately ${Math.round(elapsed / msPerDay)} days ago`;
   } else if (elapsed < msPerYear) {
-    return 'approximately ' + Math.round(elapsed / msPerMonth) + ' months ago';
+    return `approximately ${Math.round(elapsed / msPerMonth)} months ago`;
   } else {
-    return 'approximately ' + Math.round(elapsed / msPerYear) + ' years ago';
+    return `approximately ${Math.round(elapsed / msPerYear)} years ago`;
   }
 }
 
