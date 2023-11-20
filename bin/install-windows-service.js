16d7bdf63250bd38e2994c0fb65cc04816a8854c
Damian Senn <damian.senn@gmail.com>
2023-11-20 17:20:35 +0100

    chore: update windows service installation

---
+++ bin/install-windows-service.js
@@ -0,0 +1,34 @@
+const Service = require('node-windows').Service;
+const path = require('path');
+
+var svc = new Service({
+  name: 'omdash-client',
+  description: 'OmDash Client daemon',
+  script: path.join(__dirname, '../apps/omdash-client/build/client.js'),
+  env: [
+    {
+      name: 'NODE_ENV',
+      value: 'production',
+    },
+    {
+      name: 'OMDASH_SERVER_HOST',
+      value: 'ompi:3200',
+    },
+  ],
+});
+
+if (process.argv[2] === 'uninstall') {
+  svc.on('uninstall', function () {
+    console.log('Uninstall complete.');
+    console.log('The service exists: ', svc.exists);
+  });
+
+  svc.uninstall();
+  return;
+} else {
+  svc.on('install', function () {
+    svc.start();
+  });
+
+  svc.install();
+}
